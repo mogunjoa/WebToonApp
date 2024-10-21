fdcebe6af42067bf92114506d65d14aae4f3d80c
seungohjin <corrobolea@gmail.com>
2024-10-21 16:20:17 +0900

    first commit

---
+++ app/src/main/java/com/mogun/webtoonapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.mogun.webtoonapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mogun.webtoonapp.databinding.ActivityMainBinding
+import com.mogun.webtoonapp.ui.theme.WebToonAppTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
